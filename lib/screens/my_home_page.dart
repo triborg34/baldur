791dc1423147fa49b9fdcf66c185561b85d380da
triborg34 <aref.mousavi0914@gmail.com>
2026-06-09 21:39:39 +0330

    final for now

--- lib/screens/my_home_page.dart
+++ lib/screens/my_home_page.dart
@@ -57,180 +57,182 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        actions: [
-          _isPreparing
-              ? Icon(Icons.close, color: Colors.red)
-              : IconButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder:
-                          (context) => PdfViewerPage(
-                            pdfPath: _pdfLocalPath!,
-                            initalpage: 1,
-                            title: 'All',
-                          ),
-                    ),
-                  );
-                },
-                icon: Icon(Icons.book_rounded, color: Colors.white),
-              ),
-          Padding(
-            padding: const EdgeInsets.only(right: 8.0),
-            child: IconButton(
-              icon: Icon(Icons.search, color: Colors.white),
-              onPressed:
-                  () => Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => ChapterSearchPage(pdfLocalPath: _pdfLocalPath!,),
-                    ),
-                  ),
-            ),
-          ),
-        ],
-        title: const Text(
-          'Dm Guilde To Baldur"s Gate',
-          style: TextStyle(color: Colors.white),
-        ),
-        backgroundColor: Colors.blue,
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // _isPreparing
-              //     ? Text("Pepering The Pdf")
-              //     : InkWell(
-              //       child: Text("Pdf is Loaded"),
-              //       onTap: () {
-              //         Navigator.push(
-              //           context,
-              //           MaterialPageRoute(
-              //             builder:
-              //                 (context) => PdfViewerPage(
-              //                   pdfPath: _pdfLocalPath!,
-              //                   initalpage: 1,
-              //                   title: 'All',
-              //                 ),
-              //           ),
-              //         );
-              //       },
-              //     ),
-
-              // SizedBox(height: 15),
-              Text(
-                "Chapters",
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.w900),
-              ),
-              SizedBox(height: 15),
-              Container(
-                height: 150,
-                color: Colors.transparent,
-                child: PageView.builder(
-                  pageSnapping: true,
-
-                  itemBuilder:
-                      (context, index) => InkWell(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder:
-                                  (context) => PdfViewerPage(
-                                    pdfPath: _pdfLocalPath!,
-                                    initalpage: chapters[index]["page"],
-                                    title: chapters[index]["title"],
-                                  ),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          actions: [
+            _isPreparing
+                ? Icon(Icons.close, color: Colors.red)
+                : IconButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder:
+                            (context) => PdfViewerPage(
+                              pdfPath: _pdfLocalPath!,
+                              initalpage: 1,
+                              title: 'All',
                             ),
-                          );
-                        },
-                        child: Container(
-                          height: 150,
-                          margin: EdgeInsets.symmetric(horizontal: 5),
-                          decoration: BoxDecoration(
-                            color: chcolors[index],
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-                          child: Center(
-                            child: Text(
-                              chapters[index]['title'],
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18,
+                      ),
+                    );
+                  },
+                  icon: Icon(Icons.book_rounded, color: Colors.white),
+                ),
+            Padding(
+              padding: const EdgeInsets.only(right: 8.0),
+              child: IconButton(
+                icon: Icon(Icons.search, color: Colors.white),
+                onPressed:
+                    () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ChapterSearchPage(pdfLocalPath: _pdfLocalPath!,),
+                      ),
+                    ),
+              ),
+            ),
+          ],
+          title: const Text(
+            'Dm Guide To Baldur"s Gate',
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.blue,
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // _isPreparing
+                //     ? Text("Pepering The Pdf")
+                //     : InkWell(
+                //       child: Text("Pdf is Loaded"),
+                //       onTap: () {
+                //         Navigator.push(
+                //           context,
+                //           MaterialPageRoute(
+                //             builder:
+                //                 (context) => PdfViewerPage(
+                //                   pdfPath: _pdfLocalPath!,
+                //                   initalpage: 1,
+                //                   title: 'All',
+                //                 ),
+                //           ),
+                //         );
+                //       },
+                //     ),
+      
+                // SizedBox(height: 15),
+                Text(
+                  "Chapters",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.w900),
+                ),
+                SizedBox(height: 15),
+                Container(
+                  height: 150,
+                  color: Colors.transparent,
+                  child: PageView.builder(
+                    pageSnapping: true,
+      
+                    itemBuilder:
+                        (context, index) => InkWell(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder:
+                                    (context) => PdfViewerPage(
+                                      pdfPath: _pdfLocalPath!,
+                                      initalpage: chapters[index]["page"],
+                                      title: chapters[index]["title"],
+                                    ),
+                              ),
+                            );
+                          },
+                          child: Container(
+                            height: 150,
+                            margin: EdgeInsets.symmetric(horizontal: 5),
+                            decoration: BoxDecoration(
+                              color: chcolors[index],
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            child: Center(
+                              child: Text(
+                                chapters[index]['title'],
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                ),
                               ),
                             ),
                           ),
                         ),
-                      ),
-
-                  itemCount: chapters.length,
+      
+                    itemCount: chapters.length,
+                  ),
                 ),
-              ),
-              SizedBox(height: 15),
-              Text(
-                "Baldur's Gate Gazetteer",
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.w900),
-              ),
-              SizedBox(height: 15),
-              SizedBox(
-                height: 100,
-
-                child: CoustumListViewSeperated(data: gazetteer, indexcolor: 0,pdfLocalPath: _pdfLocalPath!,),
-              ),
-              SizedBox(height: 15),
-              Text(
-                "Upper City",
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.w900),
-              ),
-              SizedBox(height: 15),
-              SizedBox(
-                height: 100,
-
-                child: CoustumListViewSeperated(
-                  pdfLocalPath: _pdfLocalPath!,
-                  data: upperCity,
-                  indexcolor: 129,
+                SizedBox(height: 15),
+                Text(
+                  "Baldur's Gate Gazetteer",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.w900),
                 ),
-              ),
-              SizedBox(height: 15),
-              Text(
-                "Lower city",
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.w900),
-              ),
-              SizedBox(height: 15),
-              SizedBox(
-                height: 100,
-
-                child: CoustumListViewSeperated(
-                      pdfLocalPath: _pdfLocalPath!,
-                  data: lowerCity,
-                  indexcolor: 457,
+                SizedBox(height: 15),
+                SizedBox(
+                  height: 100,
+      
+                  child: CoustumListViewSeperated(data: gazetteer, indexcolor: 0,pdfLocalPath: _pdfLocalPath!,),
                 ),
-              ),
-              SizedBox(height: 15),
-              Text(
-                "Outer  City",
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.w900),
-              ),
-              SizedBox(height: 15),
-              SizedBox(
-                height: 100,
-
-                child: CoustumListViewSeperated(
-                      pdfLocalPath: _pdfLocalPath!,
-                  data: outerCity,
-                  indexcolor: 217,
+                SizedBox(height: 15),
+                Text(
+                  "Upper City",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.w900),
                 ),
-              ),
-            ],
+                SizedBox(height: 15),
+                SizedBox(
+                  height: 100,
+      
+                  child: CoustumListViewSeperated(
+                    pdfLocalPath: _pdfLocalPath!,
+                    data: upperCity,
+                    indexcolor: 129,
+                  ),
+                ),
+                SizedBox(height: 15),
+                Text(
+                  "Lower city",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.w900),
+                ),
+                SizedBox(height: 15),
+                SizedBox(
+                  height: 100,
+      
+                  child: CoustumListViewSeperated(
+                        pdfLocalPath: _pdfLocalPath!,
+                    data: lowerCity,
+                    indexcolor: 457,
+                  ),
+                ),
+                SizedBox(height: 15),
+                Text(
+                  "Outer  City",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.w900),
+                ),
+                SizedBox(height: 15),
+                SizedBox(
+                  height: 100,
+      
+                  child: CoustumListViewSeperated(
+                        pdfLocalPath: _pdfLocalPath!,
+                    data: outerCity,
+                    indexcolor: 217,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
